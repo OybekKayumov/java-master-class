128088b46a1db0b8a81ea9b00a1668074498a039
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-10-14 09:28:00 +0500

    Add composition

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+        SmartKitchen kithcen = new SmartKitchen();
+
+        kithcen.getDishWasher().setHasWorkToDo(true);
+        kithcen.getIceBox().setHasWorkToDo(true);
+        kithcen.getBrewMaster().setHasWorkToDo(true);
+
+        kithcen.getDishWasher().doDishes();
+        kithcen.getIceBox().orderFood();
+        kithcen.getBrewMaster().brewCoffee();
     }
 }
--- src/SmartKitchen.java
+++ src/SmartKitchen.java
@@ -1,7 +1,75 @@
 public class SmartKitchen {
 
     private CoffeeMaker brewMaster;
-    private Refrigirator iseBox;
-    private DishWacher dishWacher;
+    private Refrigerator iceBox;
+    private DishWasher dishWasher;
 
+    public SmartKitchen() {
+        brewMaster = new CoffeeMaker();
+        iceBox = new Refrigerator();
+        dishWasher = new DishWasher();
+    }
+
+    public CoffeeMaker getBrewMaster() {
+        return brewMaster;
+    }
+
+    public Refrigerator getIceBox() {
+        return iceBox;
+    }
+
+    public DishWasher getDishWasher() {
+        return dishWasher;
+    }
+}
+
+class CoffeeMaker {
+
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void brewCoffee() {
+
+        if (hasWorkToDo) {
+            System.out.println("Brewing Coffee");
+            hasWorkToDo = false;
+        }
+    }
+}
+
+class Refrigerator {
+
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void orderFood() {
+
+        if (hasWorkToDo) {
+            System.out.println("Ordering Food");
+            hasWorkToDo = false;
+        }
+    }
+}
+
+class DishWasher {
+
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void doDishes() {
+
+        if (hasWorkToDo) {
+            System.out.println("Washing Dishes");
+            hasWorkToDo = false;
+        }
+    }
 }
