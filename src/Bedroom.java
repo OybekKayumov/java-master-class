699180cd0965130b4168cdac7392dcca3d68482a
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-10-14 10:20:53 +0500

    Add composition

--- src/Bedroom.java
+++ src/Bedroom.java
@@ -12,4 +12,73 @@ public class Bedroom {
 
 }
 
+public class Wall {
+    private String direction;
 
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
+
+public class Ceiling {
+    private int height;
+    private int paintedColor;
+
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
+
+public class Bed {
+
+    private String style;
+    private int pillows;
+    private int height;
+    private int sheets;
+    private int quilts;
+
+    public Bed(String style, int pillows, int height, int sheets, int quilts) {
+        this.style = style;
+        this.pillows = pillows;
+        this.height = height;
+        this.sheets = sheets;
+        this.quilts = quilts;
+    }
+
+    public void make() {
+        System.out.println("Bed -> Making | ");
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public int getPillows() {
+        return pillows;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getSheets() {
+        return sheets;
+    }
+
+    public int getQuilts() {
+        return quilts;
+    }
+}
