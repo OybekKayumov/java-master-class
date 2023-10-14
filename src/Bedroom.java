7283444f07e8ada5f27edbe33d1aa0e26fca7211
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-10-14 10:27:01 +0500

    Add composition

--- src/Bedroom.java
+++ src/Bedroom.java
@@ -9,7 +9,26 @@ public class Bedroom {
     private Bed bed;
     private Lamp lamp;
 
+    public Bedroom(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4,
+                   Ceiling ceiling, Bed bed, Lamp lamp) {
+        this.name = name;
+        this.wall1 = wall1;
+        this.wall2 = wall2;
+        this.wall3 = wall3;
+        this.wall4 = wall4;
+        this.ceiling = ceiling;
+        this.bed = bed;
+        this.lamp = lamp;
+    }
 
+    public Lamp getLamp() {
+        return lamp;
+    }
+
+    public void makeBed() {
+        System.out.println("Bedroom -> MAking bed |");
+        bed.make();
+    }
 }
 
 public class Wall {
@@ -80,5 +99,29 @@ public class Bed {
 
     public int getQuilts() {
         return quilts;
+    }
+}
+
+public class Lamp {
+    private String style;
+    private boolean battery;
+    private int globRating;
+
+    public Lamp(String style, boolean battery, int globRating) {
+        this.style = style;
+        this.battery = battery;
+        this.globRating = globRating;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public boolean isBattery() {
+        return battery;
+    }
+
+    public int getGlobRating() {
+        return globRating;
     }
 }
