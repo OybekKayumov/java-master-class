8eb4cbb92d83e5a2f5ec9911e2e5d3460b8808f4
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-10-14 12:01:14 +0500

    Add encapsulation

---
+++ encapsulation/src/EnhancedPlayer.java
@@ -0,0 +1,47 @@
+public class EnhancedPlayer {
+
+    private String name;
+    private int health;
+    private String weapon;
+
+    public EnhancedPlayer(String name) {
+        // this.name = name;
+        this(name, 100, "Sword");
+    }
+
+    public EnhancedPlayer(String name, int health, String weapon) {
+        this.name = name;
+
+        // this.health = health;
+        if (health <=0 ) {
+            this.health = 1;
+        } else if (health > 100) {
+            this.health = 100;
+        } else {
+            this.health = health;
+        }
+
+        this.weapon = weapon;
+    }
+
+    public void loseHealth(int damage) {
+
+        health = health - damage;
+        if (health <= 0) {
+            System.out.println("Player knocked out of game");
+        }
+    }
+
+    public int healthRemaining() {
+        return health;
+    }
+
+    public void restoreHealth(int extraHealth) {
+        health = health + extraHealth;
+
+        if (health > 100) {
+            System.out.println("Player restored to 100%");
+            health = 100;
+        }
+    }
+}
--- encapsulation/src/Main.java
+++ encapsulation/src/Main.java
@@ -2,21 +2,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Player player = new Player();
-        // player.name = "Tim";
-        player.name = "Tim";
-        player.health = 20;
-        player.weapon = "Sword";
+//        Player player = new Player();
+//        player.name = "Tim";
+//        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+//        //* Remaining health = 10
+//
+//        player.health = 200;
+//
+//        player.loseHealth(11);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+//        //* Remaining health = 189
 
-        int damage = 10;
-        player.loseHealth(damage);
-        System.out.println("Remaining health = " + player.healthRemaining());
-        //* Remaining health = 10
+        // EnhancedPlayer tim = new EnhancedPlayer("Tim");
+        EnhancedPlayer tim = new EnhancedPlayer("Tim", 200, "Sword");
+        System.out.println("Initial health is " + tim.healthRemaining() );
+        //* Initial health is 100
 
-        player.health = 200;
 
-        player.loseHealth(11);
-        System.out.println("Remaining health = " + player.healthRemaining());
-        //* Remaining health = 189
     }
 }
