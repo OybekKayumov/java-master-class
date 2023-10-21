be2b3c102ac1bdb54d93f93d058b4932e8c34b6a
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-10-21 08:45:21 +0500

    Add Arrays

--- intellij-tips-and-tricks/src/MultipleArrays.java
+++ intellij-tips-and-tricks/src/MultipleArrays.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Objects;
 
 public class MultipleArrays  {
 
@@ -46,6 +47,46 @@ public class MultipleArrays  {
         System.out.println(Arrays.deepToString(array2));
         // [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
         // [[1, 2, 3, 4], [11, 12, 13, 14], [21, 22, 23, 24], [31, 32, 33, 34]]
+
+        //todo_ Multi-dimensional Arrays
+        array2[1] = new int[] {10, 20, 30};
+        System.out.println(Arrays.deepToString(array2));
+        // [[1, 2, 3, 4], [10, 20, 30], [21, 22, 23, 24], [31, 32, 33, 34]]
+
+        Object[] anyArray = new Object[3];
+        System.out.println(Arrays.toString(anyArray));
+        // [null, null, null]
+
+        anyArray[0] = new String[] {"a", "b", "c"};
+        System.out.println(Arrays.deepToString(anyArray));
+        // [[a, b, c], null, null]
+
+        anyArray[1] = new String[][]{
+                {"1", "2"},
+                {"3", "4", "5"},
+                {"6", "7", "8", "9"}
+        };
+        System.out.println(Arrays.deepToString(anyArray));
+        // [[a, b, c], [[1, 2], [3, 4, 5], [6, 7, 8, 9]], null]
+
+        anyArray[2] = new int[2][2][2];
+        System.out.println(Arrays.deepToString(anyArray));
+        // [[a, b, c], [[1, 2], [3, 4, 5], [6, 7, 8, 9]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
+
+        for (Object elem : anyArray) {
+            System.out.println("Element type: " + elem.getClass().getSimpleName());
+            System.out.println("Element to String: " + elem);
+            System.out.println(Arrays.deepToString((Object[]) elem));
+        }
+//        Element type: String[]
+//        Element to String: [Ljava.lang.String;@7699a589
+//                [a, b, c]
+//        Element type: String[][]
+//        Element to String: [[Ljava.lang.String;@58372a00
+//                [[1, 2], [3, 4, 5], [6, 7, 8, 9]]
+//        Element type: int[][][]
+//        Element to String: [[[I@378bf509
+//                [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]
 
     }
 }
